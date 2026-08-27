18a1badc83877e1041cd7023d2cdb206451e501e
Eduardo Veras <eduhveras@gmail.com>
2026-08-27 19:44:29 -0300

    Inicio do exercicio

---
+++ src/main/java/com/exercicio2/questao1/Questao1Application.java
@@ -0,0 +1,13 @@
+package com.exercicio2.questao1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Questao1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Questao1Application.class, args);
+	}
+
+}
